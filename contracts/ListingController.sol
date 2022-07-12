74d33df22003b2497f6e631c1ca69973fcd2d272
0xmadsage <zachrstolz@gmail.com>
2022-07-12 12:41:24 -0400

    ListingController module created

---
+++ contracts/ListingController.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.9;
+
+contract ListingController {
+    address internal _owner;
+
+    // Only should be created by a contract
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != _owner) revert("not owner");
+        _;
+    }
+
+    // Callee needs to be restricted
+    function execute(
+        address callee,
+        uint256 value,
+        bytes calldata data
+    ) external onlyOwner returns (bool success, bytes memory returnData) {
+        if (callee.code.length == 0) revert("not contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (success, returnData) = callee.call{ value: value }(data);
+    }
+
+    // NOTE: The gallery should always destroy the `ListingController` in the same runtime context that deploys it.
+    function destroy(address etherDestination) external onlyOwner {
+        selfdestruct(payable(etherDestination));
+    }
+
+    // // solhint-disable-next-line no-empty-blocks
+    // receive() external payable {}
+}
